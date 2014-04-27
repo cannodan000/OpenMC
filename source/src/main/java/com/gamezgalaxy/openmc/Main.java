1d956f37fd4429f88ae22926d8bddef444b31a44
Eddie <eddie@gamezgalaxy.com>
2014-04-27 18:13:56 -0400

    Added pullRepo method

--- source/src/main/java/com/gamezgalaxy/openmc/Main.java
+++ source/src/main/java/com/gamezgalaxy/openmc/Main.java
@@ -4,13 +4,18 @@ import com.gamezgalaxy.openmc.system.FileUtils;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static GHRepository OpenMC;
+    public static List<String> pullIgnores = new ArrayList<String>();
     public static void main(String[] args) {
         System.out.println("Reading from passsword file..");
         try {
@@ -24,10 +29,37 @@ public class Main {
             System.out.println("Getting repo..");
             OpenMC = github.getRepository("GamezGalaxy2/OpenMC");
 
+            System.out.println("Reading .pullignore file..");
+            pullIgnores = FileUtils.readToList(".pullignore");
 
+            System.out.println("Pull latest changes..");
+            pullRepo();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
+        }
+
+        System.out.println("Entering main loop!");
+        while (true) {
+
+        }
+    }
+
+    /**
+     * Pull the repo for new changes
+     */
+    public static void pullRepo() throws IOException {
+        Process gitProcess = Runtime.getRuntime().exec("git fetch && git reset --hard origin/master");
+        BufferedReader input = new BufferedReader(new InputStreamReader(gitProcess.getInputStream()));
+        String line;
+        while ((line = input.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        BufferedReader input2 = new BufferedReader(new InputStreamReader(gitProcess.getErrorStream()));
+        String line2;
+        while ((line2 = input2.readLine()) != null) {
+            System.out.println(line2);
         }
     }
 }
